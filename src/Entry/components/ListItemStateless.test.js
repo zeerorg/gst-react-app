55f506f29e8aa6fa95b1f82704b3a72864e5cdf6
zeerorg <r.g.gupta@outlook.com>
2018-03-17 00:51:28 +0000

    Added first test

---
+++ src/Entry/components/ListItemStateless.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import ListItem from './ListItemStateless';
+import Entry from '../entry_model';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("ListItem", () => {
+  let props;
+  let mounted;
+  const listItem = () => {
+    if (!mounted) {
+      mounted = shallow(
+        <ListItem {...props} />
+      );
+    }
+    return mounted;
+  }
+
+  beforeEach(() => {
+    props = {
+      entry: new Entry('A_Entry_E', 'A_Sheet_S', 12, "B2B", "07AAHPJ9323N1ZA", "12", new Date(), "Regular B2B", "Delhi", 27494, 23300, 18, 0, 2097, 2097)
+    };
+    mounted = undefined;
+  });
+  
+  // All tests will go here
+  it("tr is always mounted", () => {
+    const trs = listItem().find("tr");
+    expect(trs.length).toBeGreaterThan(0);
+  });
+});
